4d9c7cdc131e9262658166f757508536cc6d3c59
ZaheerUDin957 <127579823+ZaheerUDin957@users.noreply.github.com>
2024-08-12 15:56:57 +0500

    app.py

--- app.py
+++ app.py
@@ -32,7 +32,23 @@ nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 # Load the Spacy model
-nlp = spacy.load("en_core_web_sm")
+import spacy
+from spacy.cli import download
+
+# Function to ensure the model is downloaded
+def ensure_model():
+    try:
+        spacy.load("en_core_web_md")
+    except OSError:
+        print("Model not found. Downloading...")
+        download("en_core_web_md")
+
+# Ensure the model is downloaded
+ensure_model()
+
+# Load the model
+nlp = spacy.load("en_core_web_md")
+
 
 # Cleaned corpus as provided
 corpus = [
