eeb10edf45471338f28d84a97050774d37d90892
ZaheerUDin957 <127579823+ZaheerUDin957@users.noreply.github.com>
2024-08-12 15:53:56 +0500

    setup_model.py

--- setup_model.py
+++ setup_model.py
@@ -1,1 +1,14 @@
+import spacy
+from spacy.cli import download
+
+# Function to ensure the model is downloaded
+def ensure_model():
+    try:
+        spacy.load("en_core_web_md")
+    except OSError:
+        print("Model not found. Downloading...")
+        download("en_core_web_md")
+
+# Ensure the model is downloaded
+ensure_model()
 
